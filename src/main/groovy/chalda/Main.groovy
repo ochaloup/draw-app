e9eb65d48b1b88b09130f8ff92716f747c284e75
Ondrej Chaloupka <ochaloup@redhat.com>
2013-10-17 13:10:21 +0200

    Quite specific Main class settings

--- src/main/groovy/chalda/Main.groovy
+++ src/main/groovy/chalda/Main.groovy
@@ -1,11 +1,22 @@
 package chalda
 
 class Main {
+	def static INPUT_FILE = System.getProperty("DRAW_INPUT", "draw.txt")
+	def static ADMIN_MAIL = System.getProperty("DRAW_ADMIN_EMAIL", "gandalf@istari.home")
+	def static SUBJECT = System.getProperty("DRAW_SUBJECT", "A christmas draw")  
+  
+  def static IS_SEND = true;
+	
 	public static void main(String[] args) {
-		def adminEmail = System.getProperty("admin.email", "somebody")
-		
+		if (args.length > 0) {
+      INPUT_FILE = args[0].toString()
+    }
+    if (args.length > 1) {
+      ADMIN_MAIL = args[1].toString()
+    }
+        
 		def parser = new FileParser("|")
-		List parsedList = parser.parse("christmas-draw.txt")
+		List parsedList = parser.parse(INPUT_FILE)
 		
 		def dataPreparer = new DataPreparer()
 		List toDraw = dataPreparer.prepare(parsedList)
@@ -15,18 +26,37 @@ class Main {
 		
 		Util.logMatches(toDraw)
 		
-		String toAdminEmail = "The draw was done with following results:\n"
+		def mailer = new SMTPMailer("", "", "smtp.gmail.com")
+		
+		
+		def adminMail = "Draw results\n\n" << "Persons in the draw:\n"
+		adminMail << "ID".padLeft(3) << "|" << "NAME".padLeft(10) << "|" << "E-MAILS".padLeft(30) << "|" << "DEPENDENCIES" << "\n"
 		toDraw.each { p ->
 			def depnames = Util.getNamesByDep(p, toDraw)
-			toAdminEmail += "For: " + p.name + " (with deps: ${depnames}) ->  was drawn name: " + p.foundDraw.name + "\n"
+			adminMail << p.id.toString().padLeft(3) << "|" << p.name.padLeft(10) << "|" << p.emails.toString().padLeft(30) << "|" << depnames << "(" << p.dependencies << ")\n" 
 		}
+		adminMail << "\n\nResults:\n"
+		toDraw.each { p ->
+			adminMail << "Name: " << p.name << " got person in draw: " << p.foundDraw.name + "\n"
+		}
+		sendEmail(mailer, ADMIN_MAIL, SUBJECT << " [REPORT]", adminMail)
 		
-		println("Sending email to admin\n${toAdminEmail}")
-		
+		def participantMail = "You've got person: "
+		println("Sending emails to all participants of the draw")
 		toDraw.each{ p ->
+      def emailText = participantMail << p.foundDraw.name
 			p.emails.each { email ->
-				println ("Sending email to ${p.name} ${email}")
+				sendEmail(mailer, email, SUBJECT, emailText)
 			}
 		}
 	}
+  
+  private static void sendEmail(SMTPMailer mailer, email, subject, text) {
+    if(IS_SEND) {
+      mailer.sendMail(email as String, subject as String, text as String)
+      println("Sent mail via mailer '$mailer' to '$email' with subject '$subject' and text\n---\n$text\n\n")
+    } else {
+      println("Mail would be sent but IS_SEND property is false (to '$email' with subject '$subject' and text\n---\n$text)\n\n")
+    }
+  }
 }
